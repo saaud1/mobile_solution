b3ac6bfc27e736dc8b23535a4973b8659aaf116c
saaud1 <y.904@hotmail.com>
2021-12-07 16:28:40 +0100

    one

---
+++ Mobilesolutions/app/src/main/java/com/example/mobilesolutions/Accelerometer.java
@@ -0,0 +1,54 @@
+package com.example.mobile;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class Accelerometer {
+
+    public interface Listener
+    {
+        void pnTranslation(float tx, float ty, float tz);
+    }
+    private Listener listener;
+    public void setListener(Listener l)
+    {
+        listener = l;
+    }
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private SensorEventListener sensorEventListener;
+
+    public Accelerometer(Context context)
+    {
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(listener != null)
+                {
+                    listener.pnTranslation(sensorEvent.values[0],sensorEvent.values[1],sensorEvent.values[2]);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+
+    public void register()
+    {
+        sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    public void unregister()
+    {
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
+}
---
+++ Mobilesolutions/app/src/main/java/com/example/mobilesolutions/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.mobilesolutions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.example.mobile.Accelerometer;
+import com.example.mobile.Gyroscope;
+
+public class MainActivity extends AppCompatActivity {
+
+    private com.example.mobile.Accelerometer accelerometer;
+    private com.example.mobile.Gyroscope gyroscope;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        accelerometer = new Accelerometer(this);
+        gyroscope = new Gyroscope(this);
+
+        accelerometer.setListener(new Accelerometer.Listener() {
+            @Override
+            public void pnTranslation(float tx, float ty, float tz) {
+                if (tx > 1.0f)
+                {
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                }
+                if (tx < -1.0f){
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                }
+            }
+        });
+        gyroscope.setListener(new Gyroscope.Listener() {
+            @Override
+            public void onRotation(float rx, float ry, float rz) {
+                if (rz > 1.0f){
+                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                }
+                else if(rz < -1.0f){
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        accelerometer.register();
+        gyroscope.register();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        accelerometer.unregister();
+        gyroscope.unregister();
+    }
+}
